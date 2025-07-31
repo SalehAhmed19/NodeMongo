92400603c5a75228317b0c90c4003a1730354bbd
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-07-31 15:18:18 +0600

    basic-express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listenig on port ${PORT}`);
+});
