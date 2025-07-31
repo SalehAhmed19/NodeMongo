0e0676ceee8e1a08a4452acaf8c990e855b39d04
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-07-31 16:34:44 +0600

    simple mongoose connection

--- client/script2.js
+++
@@ -1,3 +0,0 @@
-var b = 10;
-
-console.log(a + b);
--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
+
+// app initialization
+const app = express();
+app.use(express.json());
+
+// db connection
+mongoose
+  .connect("mongodb://localhost:todos")
+  .then(() => {
+    console.log("Connection succeeded");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// default error handler
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err });
+};
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
